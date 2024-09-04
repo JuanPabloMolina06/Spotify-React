9d92faa8fc40b0b8b0ff1dd12f21bf6b42f4aef0
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 10:56:59 -0300

    prj

---
+++ Spotify/src/App.jsx
@@ -0,0 +1,109 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='h-screen'>
+        <header>
+          <div className='w-full h-32 bg-red-500 text-red-500'>
+            .
+          </div>
+        </header>
+
+        <main>
+            <div className='w-full h-[750px] bg-red-200 flex flex-row'>
+
+
+
+              <nav className='w-60 h-[750px] bg-cyan-400 text-cyan-400 flex flex-col justify-around items-center'>
+                
+                <div className='w-28 h-28 bg-red-600 text-red-600'>
+                  .
+                </div>
+                <div className='w-28 h-28 bg-red-600 text-red-600'>
+                  .
+                </div>
+                <div className='w-28 h-28 bg-red-600 text-red-600'>
+                  .
+                </div>
+              </nav>
+
+              <div className='flex flex-col justify-between items-center w-full'>
+                <div className='flex gap-10 mt-10 justify-center items-center mb-4'>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                      <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                      <div className='w-20 h-8 bg-cyan-400' />
+                    </div>
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                </div>
+
+
+                <div className='flex gap-10 justify-center items-center mb-4'>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                      <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                      <div className='w-20 h-8 bg-cyan-400' />
+                    </div>
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                </div>
+
+
+                <div className='flex gap-10 mb-10 justify-center items-center'>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='flex flex-col items-center'>
+                      <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                      <div className='w-20 h-8 bg-cyan-400' />
+                    </div>
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                  <div className='w-28 h-28 bg-red-600 flex flex-col justify-center items-center'>
+                    <div className='w-20 h-8 bg-cyan-400 mb-4' />
+                    <div className='w-20 h-8 bg-cyan-400' />
+                  </div>
+                </div>
+              </div>
+
+                
+
+            </div>
+            
+        </main>
+    </div>
+    </>
+  )
+}
+
+export default App
